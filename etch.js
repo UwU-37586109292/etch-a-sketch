82cb5867ff849801babfcc622b210b8a9eb97b70
Zuzka <kikucisko@gmail.com>
2022-07-25 19:59:33 +0200

    Add method to generate grid based on parameter

--- etch.js
+++ etch.js
@@ -1,3 +1,15 @@
+createGrid(16);
+
 function createGrid(size) {
-    //append grid to DOM
+
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const gridCell = document.createElement('div');
+            gridCell.setAttribute('class', 'grid-element');
+            gridContainer.appendChild(gridCell);
+        }
+    }
 }
